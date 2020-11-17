dc0f639692f06a7322ac467448d54e9af64c89e6
Haruka Kotani <harukakotani28@gmail.com>
2020-11-17 14:29:46 -0800

    Student Registration Lab8

---
+++ StudentRegistrationApp/StudentRegistrationMainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace StudentRegistrationApp
+{
+    partial class StudentRegistrationMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewStudent = new System.Windows.Forms.DataGridView();
+            this.dataGridViewCourse = new System.Windows.Forms.DataGridView();
+            this.dataGridViewRegistration = new System.Windows.Forms.DataGridView();
+            this.dataGridViewDepartment = new System.Windows.Forms.DataGridView();
+            this.buttonStudent = new System.Windows.Forms.Button();
+            this.buttonCourse = new System.Windows.Forms.Button();
+            this.buttonDrop = new System.Windows.Forms.Button();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.buttonDepartment = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCourse)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRegistration)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDepartment)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewStudent
+            // 
+            this.dataGridViewStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudent.Location = new System.Drawing.Point(38, 37);
+            this.dataGridViewStudent.Name = "dataGridViewStudent";
+            this.dataGridViewStudent.RowHeadersWidth = 62;
+            this.dataGridViewStudent.RowTemplate.Height = 28;
+            this.dataGridViewStudent.Size = new System.Drawing.Size(858, 214);
+            this.dataGridViewStudent.TabIndex = 0;
+            // 
+            // dataGridViewCourse
+            // 
+            this.dataGridViewCourse.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCourse.Location = new System.Drawing.Point(38, 269);
+            this.dataGridViewCourse.Name = "dataGridViewCourse";
+            this.dataGridViewCourse.RowHeadersWidth = 62;
+            this.dataGridViewCourse.RowTemplate.Height = 28;
+            this.dataGridViewCourse.Size = new System.Drawing.Size(698, 270);
+            this.dataGridViewCourse.TabIndex = 1;
+            // 
+            // dataGridViewRegistration
+            // 
+            this.dataGridViewRegistration.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRegistration.Location = new System.Drawing.Point(23, 840);
+            this.dataGridViewRegistration.Name = "dataGridViewRegistration";
+            this.dataGridViewRegistration.RowHeadersWidth = 62;
+            this.dataGridViewRegistration.RowTemplate.Height = 28;
+            this.dataGridViewRegistration.Size = new System.Drawing.Size(952, 193);
+            this.dataGridViewRegistration.TabIndex = 2;
+            // 
+            // dataGridViewDepartment
+            // 
+            this.dataGridViewDepartment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDepartment.Location = new System.Drawing.Point(1127, 37);
+            this.dataGridViewDepartment.Name = "dataGridViewDepartment";
+            this.dataGridViewDepartment.RowHeadersWidth = 62;
+            this.dataGridViewDepartment.RowTemplate.Height = 28;
+            this.dataGridViewDepartment.Size = new System.Drawing.Size(516, 230);
+            this.dataGridViewDepartment.TabIndex = 3;
+            // 
+            // buttonStudent
+            // 
+            this.buttonStudent.Location = new System.Drawing.Point(922, 89);
+            this.buttonStudent.Name = "buttonStudent";
+            this.buttonStudent.Size = new System.Drawing.Size(95, 124);
+            this.buttonStudent.TabIndex = 4;
+            this.buttonStudent.Text = "Add Or Update Student";
+            this.buttonStudent.UseVisualStyleBackColor = true;
+            // 
+            // buttonCourse
+            // 
+            this.buttonCourse.Location = new System.Drawing.Point(874, 313);
+            this.buttonCourse.Name = "buttonCourse";
+            this.buttonCourse.Size = new System.Drawing.Size(112, 133);
+            this.buttonCourse.TabIndex = 5;
+            this.buttonCourse.Text = "Add Or Update Course";
+            this.buttonCourse.UseVisualStyleBackColor = true;
+            // 
+            // buttonDrop
+            // 
+            this.buttonDrop.Location = new System.Drawing.Point(1374, 887);
+            this.buttonDrop.Name = "buttonDrop";
+            this.buttonDrop.Size = new System.Drawing.Size(146, 57);
+            this.buttonDrop.TabIndex = 6;
+            this.buttonDrop.Text = "Drop";
+            this.buttonDrop.UseVisualStyleBackColor = true;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(1374, 783);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(146, 53);
+            this.buttonRegister.TabIndex = 7;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            // 
+            // buttonDepartment
+            // 
+            this.buttonDepartment.Location = new System.Drawing.Point(1675, 87);
+            this.buttonDepartment.Name = "buttonDepartment";
+            this.buttonDepartment.Size = new System.Drawing.Size(133, 129);
+            this.buttonDepartment.TabIndex = 8;
+            this.buttonDepartment.Text = "Add Or Update Department";
+            this.buttonDepartment.UseVisualStyleBackColor = true;
+            // 
+            // StudentRegistrationMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1873, 1158);
+            this.Controls.Add(this.buttonDepartment);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.buttonDrop);
+            this.Controls.Add(this.buttonCourse);
+            this.Controls.Add(this.buttonStudent);
+            this.Controls.Add(this.dataGridViewDepartment);
+            this.Controls.Add(this.dataGridViewRegistration);
+            this.Controls.Add(this.dataGridViewCourse);
+            this.Controls.Add(this.dataGridViewStudent);
+            this.Name = "StudentRegistrationMainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCourse)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRegistration)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDepartment)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewStudent;
+        private System.Windows.Forms.DataGridView dataGridViewCourse;
+        private System.Windows.Forms.DataGridView dataGridViewRegistration;
+        private System.Windows.Forms.DataGridView dataGridViewDepartment;
+        private System.Windows.Forms.Button buttonStudent;
+        private System.Windows.Forms.Button buttonCourse;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Button buttonDepartment;
+        public System.Windows.Forms.Button buttonDrop;
+    }
+}
+
---
+++ StudentRegistrationApp/StudentRegistrationMainForm.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StudentRegistrationCodeFirstFromDB;
+using System.Data.Entity;
+using System.Diagnostics;
+using SeedDatabaseExtensions;
+using EFControllerUtilities;
+
+namespace StudentRegistrationApp
+{
+    public partial class StudentRegistrationMainForm : Form
+    {
+        private StudentRegistrationEntities context;
+        public StudentRegistrationMainForm()
+        {
+            InitializeComponent();
+
+            this.Text = "Student Registration using AddUpdate Forms";
+
+            context = new StudentRegistrationEntities();
+            context.Database.Log = (s => Debug.Write(s));
+            context.SeedDatabase();
+            context.SaveChanges();
+
+            Student student = new Student();
+
+            this.Load += (s, e) => StudentRegistratioMainForm_Load();
+
+          
+
+            AddOrUpdateStudent addOrUpdateStudentForm = new AddOrUpdateStudent();
+            buttonStudent.Click += (s, e) => AddOrUpdateForm<Student>(dataGridViewStudent, addOrUpdateStudentForm);
+
+            AddOrUpdateDepartment addOrUpdateDepartmentForm = new AddOrUpdateDepartment();
+            buttonDepartment.Click += (s, e) => AddOrUpdateForm<Department>(dataGridViewDepartment, addOrUpdateDepartmentForm);
+
+            AddOrUpdateCourse addOrUpdateCourseForm = new AddOrUpdateCourse();
+            buttonCourse.Click += (s, e) => AddOrUpdateForm<Course>(dataGridViewCourse, addOrUpdateCourseForm);
+
+        }
+
+
+        /// <summary>
+        /// Set up all of the datagridview controls
+        /// </summary>
+        private void StudentRegistratioMainForm_Load()
+        {
+            // common setup for datagridview controls
+
+            InitializeDataGridView<Student>(dataGridViewStudent, "Students");
+            InitializeDataGridView<Department>(dataGridViewDepartment, "Departments");
+            InitializeDataGridView<Course>(dataGridViewCourse, "Courses");
+            ItitializeDataGridViewRegistration(dataGridViewRegistration);
+
+            // InitializeDataGridView<Student>(dataGridViewRegistration, "StudentCourses");
+        }
+
+        private void ItitializeDataGridViewRegistration(DataGridView dataGrid)
+        {
+            dataGrid.ReadOnly = true;
+            dataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            DataGridViewColumn[] columns = new DataGridViewColumn[] {
+                new DataGridViewTextBoxColumn() { Name = "Department", Width = 100},
+                new DataGridViewTextBoxColumn() { Name = "Course Number", Width = 100},
+                new DataGridViewTextBoxColumn() { Name = "Course Name", Width = 100 },
+                new DataGridViewTextBoxColumn() { Name = "StudentID", Width = 100},
+                new DataGridViewTextBoxColumn() { Name = "LastName", Width = 100 },
+
+            };
+
+            dataGrid.Columns.AddRange(columns);
+
+            context.Courses.Include(d => d.Students).Load();
+            Department deparmtnet = new Department();
+
+
+/*            var studentCourse = from st in context.Students
+                                join cs in context.Courses o equals cs.CourseId
+                                
+                                where st.Courses == cs.Students
+                                select new {st.StudentLastName, st.StudentId,  }
+                                
+
+            foreach (Student student in context.Students)
+                foreach (Course course in student.Courses)
+                {
+
+                     dp = context.Departments
+                     .Where(b => b.DepartmentId == course.DepartmentId)
+                     .Select(b => b.DepartmentCode);
+
+
+                }
+
+*/
+
+
+            foreach (Student student in context.Students)
+                foreach (Course course in student.Courses)
+                {
+
+                    dataGrid.Rows.Add(new string[] { course.DepartmentId.ToString(), course.CourseNumber.ToString(), course.CourseName,
+                    student.StudentId.ToString(), student.StudentLastName});
+                }
+                    
+
+        }
+
+        /// <summary>
+        /// Common generic method to initialize datagridview controls. Allows users to add and delete data,
+        /// sets the datasource, autosizes the control to the columns.
+        /// <para>
+        /// A list of columns to hide is an optional parameter. No error checking is done on this.
+        /// </para>
+        /// <para>
+        /// We could use a form to delete items, but easy to use gridview for this, so set up 
+        /// UserDeleted event.
+        /// </para>
+        /// </summary>
+        /// <typeparam name="T">Data type associated with the gridview</typeparam>
+        /// <param name="gridView">DataGridView to be initialized</param>
+        /// <param name="navProperties">Columns to be hidden in the DataGridView</param>
+        private void InitializeDataGridView<T>(DataGridView gridView, params string[] navProperties) where T : class
+        {
+            // Allow users to add/delete rows, and fill out columns to the entire width of the control
+
+            gridView.AllowUserToAddRows = false;
+            gridView.AllowUserToDeleteRows = true;
+            gridView.ReadOnly = true;
+            gridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // set the handler used to delete an item. Note use of generics.
+
+            gridView.UserDeletedRow += GridView_UserDeletedRow<T>;
+
+            // probably not needed, but just in case we have some issues
+
+            gridView.DataError += (s, e) => GridView_DataError<T>(s as DataGridView, e);
+
+            // create a binding list and set the DataSource
+
+            gridView.DataSource = SetBindingList<T>();
+
+            // columns are autocreated, but skip the navigation properties
+            // this MUST come after DataSource is set
+
+/*            foreach (string column in navProperties)
+                gridView.Columns[column].Visible = false;
+*/
+          
+        }
+
+
+        /// <summary>
+        /// Update the database after the user has deleted the row. The binding list will
+        /// have updated the local context entities, so just remove dependencies and save
+        /// changes.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GridView_UserDeletedRow<T>(object sender, DataGridViewRowEventArgs e) where T : class
+        {
+            // gridview will null out the nav properties in dependent objects, so
+            // if there is no customer or car for an order, it is safe to remove it
+            // too bad gridview and bindinglist don't do this automatically
+
+            foreach (Student student in context.Students)
+            {
+                foreach (Course course in context.Courses)
+                {
+                    if (student.Courses == null || course.Students == null)
+                        context.Students.Remove(student);
+                }
+
+            }
+
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Catch any gridview data error, log to debug and cancel any event.
+        /// Should not happen, as all of our gridviews are readonly. Might show up when items
+        /// are deleted.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="gridView"></param>
+        /// <param name="e"></param>
+        private void GridView_DataError<T>(DataGridView gridView, DataGridViewDataErrorEventArgs e)
+        {
+            Debug.WriteLine("DataError " + typeof(T) + " " + gridView.Name + " row " + e.RowIndex + " col " + e.ColumnIndex + " Context: " + e.Context.ToString());
+            e.Cancel = true;
+        }
+
+        /// <summary>
+        /// Generic method to load a table and set it to a BindingList for use
+        /// by DataGridView.
+        /// 
+        /// Can be used to reload tables from db. This is done in the generic
+        /// addupdate form processing.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        private BindingList<T> SetBindingList<T>() where T : class
+        {
+            DbSet<T> dbSet = context.Set<T>();
+
+            dbSet.Load();
+            BindingList<T> list = dbSet.Local.ToBindingList<T>();
+            return list;
+        }
+
+        /// <summary>
+        /// Generic method to display a form and then update the relevant gridviews.
+        /// </summary>
+        /// <typeparam name="T">Data type associated with the gridview</typeparam>
+        /// <param name="dataGridView">DataGridView to be updated with new data from DB</param>
+        /// <param name="form"></param>
+        private void AddOrUpdateForm<T>(DataGridView dataGridView, Form form) where T : class
+        {
+            var result = form.ShowDialog();
+
+            // form has closed
+
+            if (result == DialogResult.OK)
+            {
+                // reload the db and update the gridview
+
+                // EF will not reload if there are no additions, so we need to force a reload
+                // Use the form's Tag to get the key of the item updated, and then force the reload
+                // of just that item.
+                // If we use controller, the table is just reloaded. This is a bit more efficient,
+                // but a pain.
+
+                if (form.Tag != null)
+                {
+                    int id = (int)form.Tag;
+
+                    T entity = context.Set<T>().Find(id);
+                    context.Entry<T>(entity).Reload();
+                }
+                else dataGridView.DataSource = SetBindingList<T>();
+
+                dataGridView.Refresh();
+
+                // ALWAYS update the customer orders report, hence use of AsNoTracking()
+
+                LoadStudentRegisterView();
+            }
+
+            // do not close, as the form object will be disposed, 
+            // just hide the form (make it invisible). 
+            // 
+            // when the inputForm is opened again (ShowDialog()), the Load event will fire
+            //  and the form will be reinitialized
+
+            form.Hide();
+        }
+
+        /// <summary>
+        /// Load the CustomerOrders view from the db. We need to force a database read,
+        /// so the best way is to create a new context and get all the entities for
+        /// the CustomerOrders view in the db.
+        /// 
+        /// </summary>
+        public void LoadStudentRegisterView()
+        {
+            using (StudentRegistrationEntities registrationView = new StudentRegistrationEntities())
+            {
+                registrationView.Database.Log = (s => Debug.Write(s));
+
+                // force a read from the db, and we don't need to track these entities
+
+                dataGridViewRegistration.DataSource = registrationView.Students.AsNoTracking().ToList();
+                dataGridViewRegistration.Refresh();
+            }
+
+
+        }
+
+        /// <summary>
+        /// Save changes in the context and update the controls
+        /// </summary>
+        /// <param name="terminate"></param>
+        private void SaveChanges(bool terminate = true)
+        {
+            try
+            {
+                context.SaveChanges();
+
+                dataGridViewStudent.Refresh();
+                dataGridViewDepartment.Refresh();
+                dataGridViewCourse.Refresh();
+             
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Database operation failed - exiting: " + e.Message);
+                if (terminate)
+                {
+                    context.Dispose();
+                    Environment.Exit(1);
+                }
+            }
+        }
+
+
+    }
+}
