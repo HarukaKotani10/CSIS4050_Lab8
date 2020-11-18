f781c3bfeaa20758d3f308197925c2cfa6453a1a
Sangmin Lee <sangminhun@gmail.com>
2020-11-17 19:32:27 -0800

    Add or Update Department

--- StudentRegistrationApp/AddOrUpdateDepartment.Designer.cs
+++ StudentRegistrationApp/AddOrUpdateDepartment.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxAddOrUpdateDepartment = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDepartmentCode = new System.Windows.Forms.TextBox();
+            this.textBoxDepartmentName = new System.Windows.Forms.TextBox();
+            this.buttonAddDepartment = new System.Windows.Forms.Button();
+            this.buttonUpdateDepartment = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxAddOrUpdateDepartment
+            // 
+            this.listBoxAddOrUpdateDepartment.FormattingEnabled = true;
+            this.listBoxAddOrUpdateDepartment.Location = new System.Drawing.Point(77, 61);
+            this.listBoxAddOrUpdateDepartment.Name = "listBoxAddOrUpdateDepartment";
+            this.listBoxAddOrUpdateDepartment.Size = new System.Drawing.Size(153, 199);
+            this.listBoxAddOrUpdateDepartment.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Department Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 371);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Department Name";
+            // 
+            // textBoxDepartmentCode
+            // 
+            this.textBoxDepartmentCode.Location = new System.Drawing.Point(77, 329);
+            this.textBoxDepartmentCode.Name = "textBoxDepartmentCode";
+            this.textBoxDepartmentCode.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDepartmentCode.TabIndex = 3;
+            // 
+            // textBoxDepartmentName
+            // 
+            this.textBoxDepartmentName.Location = new System.Drawing.Point(77, 403);
+            this.textBoxDepartmentName.Name = "textBoxDepartmentName";
+            this.textBoxDepartmentName.Size = new System.Drawing.Size(201, 20);
+            this.textBoxDepartmentName.TabIndex = 4;
+            // 
+            // buttonAddDepartment
+            // 
+            this.buttonAddDepartment.Location = new System.Drawing.Point(91, 477);
+            this.buttonAddDepartment.Name = "buttonAddDepartment";
+            this.buttonAddDepartment.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddDepartment.TabIndex = 5;
+            this.buttonAddDepartment.Text = "Add";
+            this.buttonAddDepartment.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdateDepartment
+            // 
+            this.buttonUpdateDepartment.Location = new System.Drawing.Point(213, 477);
+            this.buttonUpdateDepartment.Name = "buttonUpdateDepartment";
+            this.buttonUpdateDepartment.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdateDepartment.TabIndex = 6;
+            this.buttonUpdateDepartment.Text = "Update";
+            this.buttonUpdateDepartment.UseVisualStyleBackColor = true;
+            // 
+            // AddOrUpdateDepartment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(355, 537);
+            this.Controls.Add(this.buttonUpdateDepartment);
+            this.Controls.Add(this.buttonAddDepartment);
+            this.Controls.Add(this.textBoxDepartmentName);
+            this.Controls.Add(this.textBoxDepartmentCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxAddOrUpdateDepartment);
+            this.Name = "AddOrUpdateDepartment";
             this.Text = "AddOrUpdateDepartment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxAddOrUpdateDepartment;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDepartmentCode;
+        private System.Windows.Forms.TextBox textBoxDepartmentName;
+        private System.Windows.Forms.Button buttonAddDepartment;
+        private System.Windows.Forms.Button buttonUpdateDepartment;
     }
 }
--- StudentRegistrationApp/AddOrUpdateDepartment.cs
+++ StudentRegistrationApp/AddOrUpdateDepartment.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using StudentRegistrationCodeFirstFromDB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +15,141 @@ namespace StudentRegistrationApp
 {
     public partial class AddOrUpdateDepartment : Form
     {
+        private StudentRegistrationEntities context;
+
         public AddOrUpdateDepartment()
         {
             InitializeComponent();
+
+            // register the event handlers
+            this.Load += AddorUpdateDepartmentForm_Load;
+            buttonAddDepartment.Click += ButtonAddDepartment_Click;
+            buttonUpdateDepartment.Click += ButtonUpdateDepartment_Click;
+
+            // register event handler for when a car is selected
+            listBoxAddOrUpdateDepartment.SelectedIndexChanged += (s, e) => GetDepartment();
+
+            // always dispose of the context when the form is closed.
+            this.FormClosed += (s, e) => context.Dispose();
+        }
+
+        /// <summary>
+		/// Get a selected department from the listbox
+		/// </summary>
+        private void GetDepartment()
+        {
+            if (!(listBoxAddOrUpdateDepartment.SelectedItem is Department department))
+                return;
+            textBoxDepartmentCode.Text = department.DepartmentCode;
+            textBoxDepartmentName.Text = department.DepartmentName;
+        }
+
+        /// <summary>
+        /// Update the db with the new department data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonUpdateDepartment_Click(object sender, EventArgs e)
+        {
+            // the selection contains the entity to update, so make
+            // sure it is done.
+
+            if (!(listBoxAddOrUpdateDepartment.SelectedItem is Department department))
+            {
+                MessageBox.Show("Customer to be updated must be selected");
+                return;
+            }
+
+            // update the entity
+
+            department.DepartmentCode = textBoxDepartmentCode.Text;
+            department.DepartmentName = textBoxDepartmentName.Text;
+
+            // now update the db
+
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot add Department to database" + ex.InnerException.InnerException.Message);
+                return;
+            }
+
+            this.Tag = department.DepartmentId;  // so parent form can lookup id and reload
+
+            this.DialogResult = DialogResult.OK;
+            context.Dispose();
+            Close(); // this will not dispose the form on hide!
+        }
+
+        /// <summary>
+        /// Add a department to the db
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonAddDepartment_Click(object sender, EventArgs e)
+        {
+            // get the Department data from the textboxes
+
+            Department department = new Department()
+            {
+                DepartmentCode = textBoxDepartmentCode.Text,
+                DepartmentName = textBoxDepartmentName.Text,
+            };
+
+           
+            // now update the db
+
+            try
+            {
+                context.Departments.Add(department);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot add Department to database" + ex.InnerException.InnerException.Message);
+                return;
+            }
+
+            // if everyting is ok, get rid of the context, and close the form.
+
+            this.DialogResult = DialogResult.OK;
+            context.Dispose();
+            Close(); // this will not dispose the form on hide!
+        }
+
+        /// <summary>
+		/// The form is initially created, but loaded each time it is shown.
+		/// So make sure the context is created in the Load event.
+		/// 
+		/// This is the handler for the Load event.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+        private void AddorUpdateDepartmentForm_Load(object sender, EventArgs e)
+        {
+            this.Tag = null;
+            // Get the context and load the inventory table
+
+            context = new StudentRegistrationEntities();
+            context.Database.Log = s => Debug.Write(s);
+            context.SaveChanges();
+            context.Departments.Load();
+
+            // bind the listbox of customers to the Customers table.
+
+            listBoxAddOrUpdateDepartment.DataSource = context.Departments.Local.ToBindingList();
+
+            // no car is selected to start
+
+            listBoxAddOrUpdateDepartment.SelectedIndex = -1;
+
+            // set all textboxes to blank
+
+            textBoxDepartmentCode.ResetText();
+            textBoxDepartmentName.ResetText();
         }
     }
 }
